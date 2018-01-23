90f1c7d1e69153ec281a07ea73328de1bb0b51cd
wenxp <wenxiaopei@fishtrip.cn>
2018-01-23 10:49:53 +0800

    Initial Commit

---
+++ FTInternational/FTInternational.h
@@ -0,0 +1,19 @@
+//
+//  FTInternational.h
+//  FTInternational
+//
+//  Created by wen xiaopei on 2018/1/23.
+//  Copyright © 2018年 wen xiaopei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTInternational.
+FOUNDATION_EXPORT double FTInternationalVersionNumber;
+
+//! Project version string for FTInternational.
+FOUNDATION_EXPORT const unsigned char FTInternationalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTInternational/PublicHeader.h>
+
+
